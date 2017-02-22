16e8bf59d0cafbade01f16e8a983681bfc7cdb34
Savanna <pinkoski@msu.edu>
2017-02-21 22:43:14 -0500

    Revised layouts for simplicity. Removed toolbar from main layouts.

--- project1-team5/app/src/main/java/edu/msu/maliklau/project1/grid.java
+++ project1-team5/app/src/main/java/edu/msu/maliklau/project1/grid.java
@@ -1,6 +1,5 @@
 package edu.msu.maliklau.project1;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
